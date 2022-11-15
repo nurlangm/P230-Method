268042ef08096e637a0fbabe23699e2f10a9daea
unknown <you@example.com>
2022-11-15 19:30:26 +0400

    Method Task

---
+++ P230-Method/Program.cs
@@ -0,0 +1,10 @@
+﻿
+static int Number(int n, int m)
+{
+    m *= 1000;
+    n += m;
+    return n;
+}
+Console.WriteLine(Number(529,3));
+
+//Qeribe metod ola biler ammaki verdiyiniz Taska gore duzgun cavab cixardir :D
